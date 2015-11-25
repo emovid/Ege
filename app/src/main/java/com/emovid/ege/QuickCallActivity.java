91378d9b730162d1aebf396f4a986f4d1f927c79
Hernawan Fa'iz Abdillah <hernawan.faiz.a@mail.ugm.ac.id>
2015-11-25 17:14:37 +0700

    Add place picker when gps button clicked

--- app/src/main/java/com/emovid/ege/QuickCallActivity.java
+++ app/src/main/java/com/emovid/ege/QuickCallActivity.java
@@ -1,11 +1,19 @@
 package com.emovid.ege;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+
 public class QuickCallActivity extends AppCompatActivity {
+    int PLACE_PICKER_REQUEST = 1;
+    Place place;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +36,26 @@ public class QuickCallActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_gps) {
+            PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+            try {
+                startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
+            } catch (GooglePlayServicesRepairableException e) {
+                e.printStackTrace();
+            } catch (GooglePlayServicesNotAvailableException e) {
+                e.printStackTrace();
+            }
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                this.place = PlacePicker.getPlace(data, this);
+            }
+        }
     }
 }
