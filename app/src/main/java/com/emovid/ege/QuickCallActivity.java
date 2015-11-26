7326ac261ff4b5be117ea3ee79ff0c451f08efa4
emovid <misbach.imaduddin@yahoo.com>
2015-11-26 09:09:45 +0700

    add tollbar widget

--- app/src/main/java/com/emovid/ege/QuickCallActivity.java
+++ app/src/main/java/com/emovid/ege/QuickCallActivity.java
@@ -2,6 +2,7 @@ package com.emovid.ege;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,6 +12,8 @@ public class QuickCallActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quick_call);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
 
     @Override
