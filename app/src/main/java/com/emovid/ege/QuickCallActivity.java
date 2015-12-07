0e6106ab39d9dc7ace98518a177da2bb2774d224
Hernawan Fa'iz Abdillah <hernawan.faiz.a@mail.ugm.ac.id>
2015-12-08 04:29:29 +0700

    Add activity method to call phone number

--- app/src/main/java/com/emovid/ege/QuickCallActivity.java
+++ app/src/main/java/com/emovid/ege/QuickCallActivity.java
@@ -1,6 +1,10 @@
 package com.emovid.ege;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -60,5 +64,21 @@ public class QuickCallActivity extends AppCompatActivity {
                 this.place = PlacePicker.getPlace(data, this);
             }
         }
+    }
+
+    public void callPhoneNumber(String number) {
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse("tel:" + number));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        startActivity(callIntent);
     }
 }
