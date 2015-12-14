81340aaa9b3bc39c51f3582b292a1d828bb1b1ee
Hernawan Fa'iz Abdillah <hernawan.faiz.a@mail.ugm.ac.id>
2015-12-14 23:08:14 +0700

    Change the way how location requested, now from GPS and wait for it indefinitely

--- app/src/main/java/com/emovid/ege/QuickCallActivity.java
+++ app/src/main/java/com/emovid/ege/QuickCallActivity.java
@@ -5,9 +5,9 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Address;
-import android.location.Criteria;
 import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
@@ -25,7 +25,7 @@ import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 
-public class QuickCallActivity extends AppCompatActivity {
+public class QuickCallActivity extends AppCompatActivity implements LocationListener {
     public static String PACKAGE_NAME;
     LatLng userLocation;
     String cityName;
@@ -53,8 +53,6 @@ public class QuickCallActivity extends AppCompatActivity {
 
     public void getUserLocation() {
         LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        String provider = service.getBestProvider(criteria, false);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -65,25 +63,16 @@ public class QuickCallActivity extends AppCompatActivity {
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
-        Location location = service.getLastKnownLocation(provider);
-        if (location == null) {
-            Log.d("[getUserLocation()]", "Location not found");
-            return;
+        Location location = service.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if(location != null && location.getTime() > Calendar.getInstance().getTimeInMillis() - 2 * 60 * 1000) {
+            assignPreciseLocation(location);
+
+            Log.d(PACKAGE_NAME, "getUserLocation().latitude :: " + userLocation.latitude);
+            Log.d(PACKAGE_NAME, "getUserLocation().longitude :: " + userLocation.longitude);
+        } else {
+            Log.d(PACKAGE_NAME, "getUserLocation() :: " + "Location not found");
+            service.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) this);
         }
-        userLocation = new LatLng(location.getLatitude(), location.getLongitude());
-
-        Geocoder gcd = new Geocoder(this.getApplicationContext(), Locale.getDefault());
-        List<Address> adrs;
-
-        try {
-            adrs = gcd.getFromLocation(userLocation.latitude, userLocation.longitude, 1);
-            cityName = adrs.get(0).getLocality();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Log.d("[Location latitude]", "" + userLocation.latitude);
-        Log.d("[Location longitude]", "" + userLocation.longitude);
     }
 
     @Override
@@ -123,5 +112,41 @@ public class QuickCallActivity extends AppCompatActivity {
             return;
         }
         startActivity(callIntent);
+    }
+
+    private void assignPreciseLocation(Location location) {
+        userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+
+        // Get location user-friendly name
+        Geocoder gcd = new Geocoder(this.getApplicationContext(), Locale.getDefault());
+        List<Address> adrs;
+
+        try {
+            adrs = gcd.getFromLocation(userLocation.latitude, userLocation.longitude, 1);
+            cityName = adrs.get(0).getLocality();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        myToolbar.setTitle("Ege on " + cityName);
+        assignPreciseLocation(location);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
