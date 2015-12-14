631c84d95e6cd358682aef31e836dee979326ab4
Hernawan Fa'iz Abdillah <hernawan.faiz.a@mail.ugm.ac.id>
2015-12-15 06:51:32 +0700

    Fix cityName empty

--- app/src/main/java/com/emovid/ege/QuickCallActivity.java
+++ app/src/main/java/com/emovid/ege/QuickCallActivity.java
@@ -43,7 +43,7 @@ public class QuickCallActivity extends AppCompatActivity implements LocationList
 
     // Location Store
     LatLng userLocation;
-    String cityName;
+    String cityName = "";
 
     SpotEgeFilterableList spoteges = new SpotEgeFilterableList();
 
